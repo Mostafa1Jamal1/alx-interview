ee5245ae706466d42c2c25e7bc5463f887ce8e74
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-05-03 04:02:09 +0300

    task 0: Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Lockboxes interview question '''
+
+
+def canUnlockAll(boxes):
+    ''' determines if all the boxes can be opened. '''
+    keys = {0}
+    opened = set()
+    while (len(keys) > len(opened)):
+        for key in (keys - opened):
+            opened.add(key)
+            keys.update(boxes[key])
+
+        for key in keys:
+            if (key > len(boxes)):
+                keys.pop(key)
+
+        if (len(opened) == len(boxes)):
+            return True
+
+    return False
