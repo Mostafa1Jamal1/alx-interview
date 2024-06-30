10cbb2d2a162d68ec748dde1e0fbde4eae076eec
Mostafa Jamal <mostafa.jamal.mjm@gmail.com>
2024-06-30 23:56:56 +0300

    fix: up to 4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -38,7 +38,7 @@ def validUTF8(data):
         while x & byte == x:
             x = x >> 1
             n_ones += 1
-            if n_ones > 4:
+            if n_ones >= 4:
                 return False
     if n_ones > 0:
         return False
